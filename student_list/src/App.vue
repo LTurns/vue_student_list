0f2ca12c819f86d2a7dd8dfef9bdc4e4c4fb329b
LTurns <lizzieturney@gmail.com>
2020-08-10 11:12:17 +0100

    initial structure of student list created

---
+++ student_list/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <nav class="navbar navbar-dark bg-primary justify-content-between flex-nowrap flex-row">
+      <div class="container">
+        <a class="navbar-brand float-left">MEVN Stack Example</a>
+        <ul class="nav navbar-nav flex-row float-right">
+          <li class="nav-item">
+      <router-link class="nav-link pr-3" to="/">Create Student</router-link>
+      </li>
+      <li>
+      <router-link class="nav-link" to="/view">View Students</router-link>
+      </li>
+      </ul>
+    </div>
+    </nav>
+    <div class="container mt-5">
+    <router-view/>
+    </div>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ student_list/src/components/ListComponent.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="row justify-content-center">
+    <div class="col-md-6">
+
+    </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
