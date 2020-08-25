5901148ef77e0359494cce1496b42a641158ddf3
LTurns <lizzieturney@gmail.com>
2020-08-25 14:24:09 +0100

    incorporating testing to vue project

---
+++ student_list/__tests__/createcomponent.spec.js
@@ -0,0 +1,25 @@
+import { mount } from '@vue/test-utils'
+// this mounts the component for you - and child components.
+
+{ shallowMount}
+// mounts only the main component - not child components. 
+// Shallow mount stubs out any child components - isolated testing.
+
+
+import CreateComponent from '../src/components/CreateComponent.vue'
+
+describe('CreateComponent', () => {
+ // Inspect the raw component options
+ it('has data', () => {
+   expect(typeof CreateComponent.data).toBe('function')
+ })
+
+ it('has methods', () => {
+  expect(typeof CreateComponent.methods).toBe('object')
+})
+
+ 
+
+})
+
+
